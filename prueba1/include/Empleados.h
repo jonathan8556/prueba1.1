fbf9f08182e52396952ba5a0b205734ff1576382
Jonathan Samuel Gonzalez <jgonzalezi7@miumg.edu.gt>
2025-04-07 23:37:08 -0600

    subida clase

---
+++ prueba1/include/Empleados.h
@@ -0,0 +1,16 @@
+#ifndef EMPLEADOS_H
+#define EMPLEADOS_H
+
+
+class Empleados
+{
+    public:
+        Empleados();
+        virtual ~Empleados();
+
+    protected:
+
+    private:
+};
+
+#endif // EMPLEADOS_H
